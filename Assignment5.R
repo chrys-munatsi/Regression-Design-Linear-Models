e35f9a4f920f8ac36de15a33424f25375dbe48da
Chrysanthemum Munatsi <christubbie@gmail.com>
2020-09-19 13:25:12 +0200

    Update Assignment5.R

--- Assignment5.R
+++ Assignment5.R
@@ -54,23 +54,70 @@ sort(tapply(frog$mass, frog$food, sd))
 
 
 # Question 6
-m1 <- aov(mass~food, data = frog)
+m1 <- aov(values~treatment)
 summary(m1)
 # (a.) 
 # What is the estimated Mean Square for Error, to three decimal places? 
-0.00264  = 0.003
+0.030
 # (b.)
 # What is the F value for the treatment effect, to two decimal places?  
-29.85
+13.21
 # (c.) 
 # What is the numerator degrees of freedom for this F test?  
-1
+3
 # (d.) 
 # What is the denominator degrees of freedom for this F test?  
-18
+16
 
 
+# Question 7
+# What are the estimated mean and standard error for treatment "10", respectively?
+m1.gm <- lm(values ~ treatment - 1)
+summary(m1.gm)
+# 0.23600 - estimate
+# 0.02131 - SE
 
+# Question 8 
+# What is the (absolute) estimated difference between treatments "10" and "60" 
+0.30000 - 0.23600
+# and the associated standard error, respectively
+
+
+# Question 9
+# What are the estimated mean and standard error for treatment "60", respectively? 
+# estimate - 0.30000
+# SE - 0.02131
+
+# Question 10
+# What is the (absolute) estimated difference between treatments "30" and "60"
+# and the associated standard error, respectively, rounded to two decimal places?  
+# estimate 0.30000-0.21600 
+
+# Question 11
+# What is the p-value corresponding to the null hypothesis that treatments "30" and "60"
+# have the same mean, rounded to two decimal places? 
+data1 =data.frame(treatment,values)
+m2 <- t.test(values ~ treatment, data = data1[data1$treatment %in% c("30", "60"),])
+# p-value = 0.006287
+
+# Question 12 
+# Summarize what you have learned about the relationship between mass at
+# metamorphosis and amount of food from the above analyses (that is, from the box plots, 
+# anova and contrasts) in your own words.
+
+
+# Question 13 
+# Use Tukey's procedure to calculate all possible pairwise contrasts then answer the following questions. All answers must be rounded to 2 decimal places.
+
+# (a.) 
+# What is the (absolute) estimated difference between treatments "30" and "60"? 
+
+ 
+# (b.) What are the lower and upper 95% confidence limits corresponding to the 
+# difference given in (a.)
+
+# (c.) 
+# What is the adjusted p-value for the comparison in (a.)? 
 library(multcompView)
 treatment <- factor(sort(rep(c(10,30,60,90),5)))
 values = c(0.29,0.24,0.22,0.24,0.19,0.25,0.23,0.20,0.23,0.17,0.34,0.24,0.33,0.29,0.3,0.41,0.49,0.37,0.38,0.29)
@@ -85,7 +132,3 @@ summary(ANOVA)
 # Tukey test to study each pair of treatment :
 TUKEY <- TukeyHSD(x=ANOVA, 'data$treatment', conf.level=0.95)
 TUKEY
-
-
-
-
