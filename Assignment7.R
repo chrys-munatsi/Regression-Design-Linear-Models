880636d91d33dab4f9f6eb200a82f36d64c7dd9c
Chrysanthemum Munatsi <christubbie@gmail.com>
2020-10-25 03:58:30 +0200

    Create Assignment7.R

---
+++ Assignment7.R
@@ -0,0 +1,19 @@
+library(MASS)
+
+eagles <- eagles 
+
+# Question 1
+# The number combinations of the factor variables across the 3 
+# variables are six
+# There are 8 observations of 5 variables.
+
+# Question 2
+# Find the average success rate for immature (pirating) eagles.
+# Create a table of prop.success and see where it lies?
+
+# Question 3
+# Which of the six groups (two pirate age groups, two pirate size groups, 2 
+victim size groups) has the lowest median success rate? I.e., what seems the 
+single most important factor in determining success or failure in a pirating attempt?
+
+# Question 4
