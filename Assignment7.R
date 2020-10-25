464a871c25fcfc9f9d311dff7449bcbea6d63ede
Chrysanthemum Munatsi <christubbie@gmail.com>
2020-10-25 04:18:04 +0200

    Update Assignment7.R

--- Assignment7.R
+++ Assignment7.R
@@ -13,7 +13,29 @@ eagles <- eagles
 
 # Question 3
 # Which of the six groups (two pirate age groups, two pirate size groups, 2 
-victim size groups) has the lowest median success rate? I.e., what seems the 
-single most important factor in determining success or failure in a pirating attempt?
+# victim size groups) has the lowest median success rate? 
+Small victim
+# I.e., what seems the single most important factor in determining success
+# or failure in a pirating attempt? 
+Size and age
 
 # Question 4
+# Which of the four categories of pirating eagle has lowest median success 
+# rate?
+Small Immature
+
+# Question 5: 
+# Attach a copy of your figure with the four boxplots (success rate per pirate 
+# eagle category).
+
+# Question 6:
+# Based on the single explanatory variable models, which single variable is 
+# the best predictor for success of a pirating attempt?
+P Size of pirating eagle (L = large, S = small)
+
+# Question 7: Assuming the same distribution for the response and the same
+# link function, are the following two models nested? η i represents the linear predictor.
+Yes
+
+# Question 8:
+# When are likelihood ratio tests in logistic regression appropriate?
