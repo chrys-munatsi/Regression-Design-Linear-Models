d370a2be053eb37cd8c945ef988f0cb7f66d957e
Chrysanthemum Munatsi <christubbie@gmail.com>
2020-10-27 04:14:30 +0200

    Update Assignment7.R

--- Assignment7.R
+++ Assignment7.R
@@ -9,7 +9,8 @@ eagles <- eagles
 
 # Question 2
 # Find the average success rate for immature (pirating) eagles.
-# Create a table of prop.success and see where it lies?
+
+
 
 # Question 3
 # Which of the six groups (two pirate age groups, two pirate size groups, 2 
@@ -27,6 +28,7 @@ Small Immature
 # Question 5: 
 # Attach a copy of your figure with the four boxplots (success rate per pirate 
 # eagle category).
+boxplot(y/n ~ A + P)
 
 # Question 6:
 # Based on the single explanatory variable models, which single variable is 
@@ -46,6 +48,11 @@ and large N ( number of observations)
 # Fully specify the model that has been fitted in model A.
 
 # Question 10: 
+mA <- glm(cbind(y, n-y)  A + P + A*P + V, family = binomial)
+mB <- glm(cbind(y, n-y)  A + P + V, family = binomial)
+mC <- glm(cbind(y, n-y)  A + P + A*P, family = binomial)
+
+
 # Look at output from model A.
 # Give residual degrees of freedom, 
 # number of parameters estimated
