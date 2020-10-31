87218c47cf95d590c9025896fd783b66ab754da2
Chrysanthemum Munatsi <christubbie@gmail.com>
2020-10-31 03:45:52 +0200

    Update Assignment7.R

--- Assignment7.R
+++ Assignment7.R
@@ -106,9 +106,10 @@ logLik(mC) = -33.23876 (df=4)
 
 # Question 17: Look at the outputs from models A and B. What would you reply to a 
 # claim that age of pirate eagle has no measurable effect on the probability of success? Justify your answer.
-I would agree with the claim because large eagles, regardless of age, were usually successful 
-in pirating from feeding eagles. The p values for age in both models A and B were 
-not statistically significant.
+I would agree with the claim because large eagles, regardless of age, were usually successful
+in pirating from feeding eagles. The small difference between the AIC values from models 
+A and B show that there is little to no measurable effect of the age of pirate eagles.  
+The p values for age in both models A and B were not statistically significant.
 
 
 # Question 18: Which of the three models (A, B, C) has most support from the data?
